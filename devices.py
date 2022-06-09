88ec915e5d967919ba486d67368b68d5556ba0fe
Denis <pydenzk@gmail.com>
2022-06-10 00:25:31 +0300

    Added Level 4

--- devices.py
+++ devices.py
@@ -5,43 +5,55 @@ class Device:
 
 
 class Notebook(Device):
-    def __init__(self, model: str, year: str, os: str, problem: str):
+    def __init__(self, model: str, year: str, os: str, problem: str, type='Ноутбук'):
         super().__init__(model, problem)
+        self.type = type
         self.year = year
         self.os = os
 
     def __str__(self):
-        return f'Тип устройства: {self.__class__.__name__}\nМодель: {self.model}\
+        return f'Тип устройства: {self.type}\nМодель: {self.model}\
         \nГод выпуска: {self.year}\nОперационная система: {self.os}\nНеисправность: {self.problem}'
 
     def __repr__(self):
-        return f'Тип устройства: {self.__class__.__name__}\nМодель: {self.model}\
+        return f'Тип устройства: {self.type}\nМодель: {self.model}\
         \nГод выпуска: {self.year}\nОперационная система: {self.os}\nНеисправность: {self.problem}'
+
+    def save(self):
+        return self.type, self.model, self.year, self.os, self.problem
 
 
 class Phone(Device):
-    def __init__(self, model: str, os: str, problem: str):
+    def __init__(self, model: str, os: str, problem: str, type='Телефон'):
         super().__init__(model, problem)
         self.os = os
+        self.type = type
 
     def __str__(self):
-        return f'Тип устройства: {self.__class__.__name__}\nМодель: {self.model}\
+        return f'Тип устройства: {self.type}\nМодель: {self.model}\
         \nОперационная система: {self.os}\nНеисправность: {self.problem}'
 
     def __repr__(self):
-        return f'Тип устройства: {self.__class__.__name__}\nМодель: {self.model}\
+        return f'Тип устройства: {self.type}\nМодель: {self.model}\
         \nОперационная система: {self.os}\nНеисправность: {self.problem}'
+
+    def save(self):
+        return self.type, self.model, self.os, self.problem, ''
 
 
 class TV(Device):
-    def __init__(self, model: str, diagonal: str, problem: str):
+    def __init__(self, model: str, diagonal: str, problem: str, type='Телевизор'):
         super().__init__(model, problem)
         self.diagonal = diagonal
+        self.type = type
 
     def __str__(self):
-        return f'Тип устройства: {self.__class__.__name__}\nМодель: {self.model}\
-        \nДиагональ: {self.diagonal}\nНеисправность: {self.problem}'
+        return f'Тип устройства: {self.type}\nМодель: {self.model}\
+        \nДиагональ: {self.diagonal}"\nНеисправность: {self.problem}'
 
     def __repr__(self):
-        return f'Тип устройства: {self.__class__.__name__}\nМодель: {self.model}\
-        \nДиагональ: {self.diagonal}\nНеисправность: {self.problem}'
+        return f'Тип устройства: {self.type}\nМодель: {self.model}\
+        \nДиагональ: {self.diagonal}"\nНеисправность: {self.problem}'
+
+    def save(self):
+        return self.type, self.model, self.diagonal, self.problem, ''
--- utils.py
+++ utils.py
@@ -1,18 +1,4 @@
-from receipt import Order
-from devices import Phone, Notebook, TV
-order_list = [
-    Order('Cat', Phone('Xiaomi', 'Android', 'разбито стекло экрана')),
-    Order('Dog', Notebook('HP', '2010', 'Windows 7', 'не загружается')),
-    Order('Cat', Notebook('Acer', '2015', 'Windows 10', 'перегрев')),
-    Order('Dog', TV('LG', '40', 'не включается')),
-    Order('Cat', TV('Samsung', '55', 'не работает Wi-Fi')),
-]
-
-def for_check_level_2():
-    print('Для проверки "вшиты" ФИО: Cat и Dog, и номера квитанций:', end=' ')
-    for el in order_list:
-        print(el.numb_receipt, end=' ')
-    print()
+order_list = []
 
 # https://pythonist.ru/heshirovanie-parolej-v-python-tutorial-po-bcrypt-s-primerami/
 login_pw = [
